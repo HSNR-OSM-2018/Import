eb09c5ec95aead9bb7674647aacae41dce6d1780
Sebastian Bair <sebastian.bair@googlemail.com>
2018-01-31 12:02:02 +0100

    Reverted changes in NodeInfo

--- src/main/java/NodeInfo.java
+++ src/main/java/NodeInfo.java
@@ -1,12 +1,10 @@
-import de.hsnr.osm2018.data.graph.Node;
-
 public class NodeInfo {
 
     private float _lat;
     private float _lon;
     private boolean _marked;
 
-    public NodeInfo(double lat, double lon){
+    public NodeInfo(double lat, double lon) {
         _lat = (float) lat;
         _lon = (float) lon;
         _marked = false;
@@ -28,7 +26,11 @@ public class NodeInfo {
         this._marked = _marked;
     }
 
-    public int distToNode(NodeInfo node){
-        return (int) Node.getDistance(this._lat, this._lon, node._lat, node._lon);
+    public int distToNode(NodeInfo node) {
+        double dist = 0;
+        double lat = 111.3 * (this._lat - node.Lat());
+        double lon = 71.5 * (this._lon - node.Lon());
+        dist = Math.sqrt(lat * lat + lon * lon);
+        return (int) (dist * 1000);
     }
 }
